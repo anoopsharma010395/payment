9b230500ef15e1bca840fa45bf330f8512fccee7
Anoop Sharma <38686430+anoopsharma010395@users.noreply.github.com>
2020-07-14 17:42:15 +0530

    Update financeApp.js

--- finance calculator/src/financeApp.js
+++ finance calculator/src/financeApp.js
@@ -3,12 +3,11 @@ import React from 'react';
 function financeApp() {
   return(
     <div id='enclosingDiv'>
-      <label id='typeLabel'>
-        <input type="select" id="selectInstrument"/>
-      </label>
+      <label id='typeLabel'>Select Type Of Instrument</label>
+      <input type="select" id="selectInstrument"/>
+      <label id='valuelabel'>Accept Terms & Conditions</label>
+      <input type="radio" id="terms_condition"/>
     </div>
-  )
-  
-}
+  )}
 
 export default FinanceApp;
