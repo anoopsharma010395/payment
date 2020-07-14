0b3b6cdcfd1d298bdcafe307b228a5b43ec809c3
Anoop Sharma <38686430+anoopsharma010395@users.noreply.github.com>
2020-07-14 17:52:06 +0530

    Update index.js

--- finance calculator/src/index.js
+++ finance calculator/src/index.js
@@ -1,6 +1,8 @@
+//All standard imports goes here.
 import React from 'react';
 import ReactDom from 'react-dom';
 
+//All project related imports goes here.
 import FinanceApp from './financeApp';
 
 
